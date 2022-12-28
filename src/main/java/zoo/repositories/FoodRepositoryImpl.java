c5b7ce52ff7bb9ed2d3c98c1a198f9fc2cd6359a
Vladimir Zorov <vl.zorov@gmail.com>
2022-12-28 19:14:58 +0200

    Initial commit

---
+++ src/main/java/zoo/repositories/FoodRepositoryImpl.java
@@ -0,0 +1,27 @@
+package zoo.repositories;
+
+import zoo.entities.foods.Food;
+
+import java.util.Collection;
+
+public class FoodRepositoryImpl implements FoodRepository{
+
+    private Collection<Food> foods;
+
+    @Override
+    public void add(Food food) {
+        foods.add(food);
+    }
+
+    @Override
+    public boolean remove(Food food) {
+        return foods.remove(food);
+    }
+
+    @Override
+    public Food findByType(String type) {
+       return foods.stream().filter(f-> f.getClass().getSimpleName().equals(type))
+                .findFirst()
+                .orElse(null);
+    }
+}
