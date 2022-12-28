95344c970fdb31265bc08ab5ea74cdf9ab024ec2
Vladimir Zorov <vl.zorov@gmail.com>
2022-12-28 23:08:54 +0200

    add

--- src/main/java/zoo/repositories/FoodRepositoryImpl.java
+++ src/main/java/zoo/repositories/FoodRepositoryImpl.java
@@ -2,12 +2,16 @@ package zoo.repositories;
 
 import zoo.entities.foods.Food;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class FoodRepositoryImpl implements FoodRepository{
 
     private Collection<Food> foods;
 
+    public FoodRepositoryImpl() {
+        this.foods = new ArrayList<>();
+    }
 
     @Override
     public void add(Food food) {
